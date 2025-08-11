71b5dba9b2c1ea5e0f8b0fb4c1db528d00d8adbc
Sahand Poursadeghi Khiavi <sahand.prs@gmail.com>
2025-08-11 11:17:29 +0200

    Created the folder and structure for new project

--- Number Guesser Game/Solution B/src/game_logic/hint_generator.py
+++
@@ -1,5 +0,0 @@
-def provide_hint(guess, actual_number):
-    if guess < actual_number:
-        print("Your guess is too low")
-    elif guess > actual_number:
-        print("youe guess is too high")    
